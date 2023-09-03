afea845c1c792a47b4e96e5bbf52e39b39b2419b
Soubhagya118 <rinkisoubhagyasamal118@gmail.com>
2023-09-03 13:35:26 +0530

    SigIn Page Functionality added with Firebase

--- src/components/Authentication/SignUp/SignUp.js
+++ src/components/Authentication/SignUp/SignUp.js
@@ -17,44 +17,45 @@ const authHandler =()=>{
         e.preventDefault();
        const email1=inputEmail.current.value;
        const password1 = inputPassword.current.value;
-       const confirmpassword1=inputConfirmPassword.current.value;
+if(isLoggin==false){
+       var confirmpassword1=inputConfirmPassword.current.value;
+}
+       let fullUrl;
+if(isLoggin){
+        fullUrl=`${url}signInWithPassword?key=${api}`
+}
+else{
+    fullUrl = `${url}signUp?key=${api}`;
+    if(confirmpassword1!=password1){
+        return alert('PLEASE CORRECT YOUR PASSWORD')
+}
+fetch(fullUrl,{
+    method:'POST',
+    headers:{
+         'Content-Type': 'application/json'
+     },
+     body:JSON.stringify({
+         email:email1,
+         password:password1,
+         returnSecureToken:true
 
-    if(confirmpassword1==password1){
-        let fullUrl;
-       if(isLoggin){
+     })
 
-       }else{
-        fullUrl = `${url}signUp?key=${api}`
-       }
-        fetch(fullUrl,{
-           method:'POST',
-           headers:{
-                'Content-Type': 'application/json'
-            },
-            body:JSON.stringify({
-                email:email1,
-                password:password1,
-                returnSecureToken:true
 
-            })
-      
-
-        })
-        
-        .then(res=>{
-            if(!res.ok){
-                throw new Error('............invalid Input')
-            }
-            return res.json()
-        }).then((data)=>{
-                console.log(data)
-        }).catch(err=>{
-            console.log(err.message)
-        })
-       
-    }else{
-        alert('PLEASE CORRECT YOUR PASSWORD')
-    }
+ })
+ 
+ .then(res=>{
+    console.log("res",res)
+     if(!res.ok){
+         throw new Error('............invalid Input')
+     }
+     return res.json()
+ }).then((data)=>{
+         console.log("user has succesfully signUp", data)
+ }).catch(err=>{
+     console.log(err.message)
+ })
+}
 }
   return (
     <div>
@@ -62,21 +63,28 @@ const authHandler =()=>{
         <div className={classes.formDiv}>
             <form className={classes.form} onSubmit={signUpFormHandler}>
             <h1>{!isLoggin?'SignUp':'LogIn'}</h1>
+          
                 <div className={classes.inputdiv}>
                 <label htmlFor='email'>Email</label>
                 <input id='email' type='email' ref={inputEmail} required/>
 
                 <label htmlFor='password'>Password</label>
                 <input id='password' type='password' ref={inputPassword} required/>
+                {!isLoggin?<>
                 <label htmlFor='confirmpassword'>Confirm Password</label>
-                <input id='confirmpassword' type='password' ref={inputConfirmPassword} required/>
+                <input id='confirmpassword' type='password' ref={inputConfirmPassword} required/></>:''}
+
+
                 </div>
+            
                 <div className={classes.inputdiv2}>
-                <button>SignUp
+                <button > {isLoggin?'Login':'SignUp'}
               </button>
                 </div>
+                {isLoggin?<a href='#'>Forget Password</a>:''}
+                
             </form>
-            <div className={classes.inputdiv3}>Have an account? Login</div>
+            <div className={classes.inputdiv3} onClick={authHandler}> {isLoggin?"Don't have an account?Sign up":'Have an account? Login'}</div>
 
         </div>
       </section>
